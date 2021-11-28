161cff8bcfa70a01d388ccca88625f4fdd05de5f
Senthil Kumar <vsenth17@ford.com>
2021-11-28 18:20:30 +0530

    using external styling and adding it to global styling

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+function About() {
+    return (
+        <div>
+            <button className='btn btn-primary'>Primary</button>
+            <button className='btn btn-success'>Secondary</button>
+        </div>
+    )
+}
+
+export default About
